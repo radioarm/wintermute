46b1c3ed755f64e4db239b6fad82cd00fe212b0c
Marcin <marcin.pietranik@gmail.com>
2022-05-26 13:02:23 +0200

    api key w header http

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from fastapi import Security, HTTPException
-from fastapi.security.api_key import APIKeyQuery
+from fastapi.security.api_key import APIKeyHeader
 from starlette.status import HTTP_403_FORBIDDEN
 
 from .config import get_settings
@@ -11,12 +11,10 @@ settings = get_settings()
 
 async def get_api_key(
     api_key_query: str = Security(
-        APIKeyQuery(name=settings.api_key_name, auto_error=False)
+        APIKeyHeader(name=settings.api_key_name, auto_error=False)
     )
 ):
     if api_key_query == settings.api_key:
         return api_key_query
     else:
-        raise HTTPException(
-            status_code=HTTP_403_FORBIDDEN,
-            detail="Invalid API key")
+        raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="Invalid API key")
--- run.py
+++ run.py
@@ -4,4 +4,4 @@ import uvicorn
 
 
 if __name__ == '__main__':
-    uvicorn.run('app.main:app', host='127.0.0.1', port=8000, log_level='info', reload=True)
+    uvicorn.run('app.main:app', host='127.0.0.1', port=8001, log_level='info', reload=True)
